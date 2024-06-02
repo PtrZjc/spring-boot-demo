6102ae2822521cbbd012f557e2e81aed749143e8
Piotr Zajac <ptr.zjc@gmail.com>
2024-06-02 16:16:32 +0200

    Add Spring Security with base security

--- src/main/java/pl/zajacp/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/pl/zajacp/springbootdemo/SpringBootDemoApplication.java
@@ -3,7 +3,19 @@ package pl.zajacp.springbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
 @SpringBootApplication
 public class SpringBootDemoApplication extends SpringBootServletInitializer {
 
@@ -11,4 +23,27 @@ public class SpringBootDemoApplication extends SpringBootServletInitializer {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
 
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/about").authenticated()
+                        .anyRequest().permitAll())
+                .formLogin(withDefaults())
+                .build();
+    }
 }
--- src/main/java/pl/zajacp/springbootdemo/api/StartController.java
+++ src/main/java/pl/zajacp/springbootdemo/api/StartController.java
@@ -2,12 +2,19 @@ package pl.zajacp.springbootdemo.api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class StartController {
 
-        @GetMapping("/")
-        public String start() {
-            return "home";
-        }
+    @GetMapping("/")
+    public String start() {
+        return "home";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    public String about() {
+        return "Here you can find some details for logged users";
+    }
 }
