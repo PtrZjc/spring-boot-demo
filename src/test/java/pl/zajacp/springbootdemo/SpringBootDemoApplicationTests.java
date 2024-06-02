fd19a5ce8025733fff0349bfe8d0c692b786b292
Piotr Zajac <ptr.zjc@gmail.com>
2024-06-02 14:03:37 +0200

    Initialize spring boot app

---
+++ src/test/java/pl/zajacp/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zajacp.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
